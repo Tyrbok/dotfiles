3b901bf4ac4797b6d6ca704300476ad363952044
Jesús Serrano <jesus.serrano@gmail.com>
2022-04-16 19:39:14 +0200

    chore(aliases): git tag function ordered by creation date

--- config/zsh/public/aliases.sh
+++ config/zsh/public/aliases.sh
@@ -15,11 +15,22 @@ alias gps="git push"
 alias gpsf="git push --force"
 alias gpl="git pull --rebase --autostash"
 alias gb="git branch"
+alias __gt="git for-each-ref --sort=-creatordate --format '%(refname) %(creatordate)' refs/tags | cut -d' ' -f1 | sed 's/refs\/tags\///g'"
+function gt {
+  re='^[0-9]+$'
+  if ! [[ $1 =~ $re ]] ; then
+    __gt
+  else
+    __gt | head -n $1
+  fi
+}
 
 alias isodate="date +%Y-%m-%dT%H:%M:%S%z"
 alias wp="ruby -I ~/projects/personal/wp-docker/lib ˜/projects/personal/wp-docker/bin/wp-docker"
 
 alias e="idea -e"
+
+alias k="kubectl"
 
 # alias clean_bbt_android="cd $HOME/projects/bitban; find . -name 'gradlew' | sed 's/gradlew//' | xargs -I % sh -c 'echo % ; echo "-------------" ; cd "%"; ./gradlew clean; echo; echo '"
 # export FZF_DEFAULT_OPTS="--preview 'bat --style=numbers --color=always --line-range :500 {}'"
