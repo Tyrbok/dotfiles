db04470caf1fe41696d1191299d7bbe71d42a6d9
Jesús Serrano <jesus.serrano@gmail.com>
2022-04-18 23:27:17 +0200

    build: installer can link directories

--- installer/restore.sh
+++ installer/restore.sh
@@ -5,6 +5,7 @@ colors
 
 cd $HOME
 PATH_ZSH_DOTFILES=$PWD/.dotfiles/config/zsh
+PATH_ITERM_DOTFILES=$PWD/.dotfiles/config/iterm2
 PATH_GIT_DOTFILES=$PWD/.dotfiles/config/git
 PATH_INSTALL_TEMPLATES=$PWD/.dotfiles/installer/_templates
 PATH_INSTALL_STEPS=$PWD/.dotfiles/installer/_steps
@@ -17,7 +18,16 @@ function _link() {
 
   print -P "$fg[white] - Link file $1 -> $2 ...$reset_color"
   ln -s $1 $2
+}
 
+function _link_path() {
+  if [ -d $2 ]; then
+    print -P "$fg[blue] - Remove path $2 ...$reset_color"
+    rm -rf "$2"
+  fi
+
+  print -P "$fg[white] - Link path $1 -> $2 ...$reset_color"
+  ln -s $1 $2
 }
 
 for step in $(ls -1 $PATH_INSTALL_STEPS | sort -t'.' -n -k1); do
