c4ad8268997d9f26bb480afc5a013ad4741e3b20
Jesús Serrano <jesus.serrano@gmail.com>
2023-08-05 18:43:03 +0200

    feat(install): prevent karabiner duplication directories

--- installer/_steps/90.karabiner.sh
+++ installer/_steps/90.karabiner.sh
@@ -1,5 +1,8 @@
 print -P "$fg[yellow]* Copy Karabiner dotfiles...$reset_color"
-
-mkdir -p $PWD/.config
-_link $PATH_KARABINER_DOTFILES/karabiner.edn $PWD/.config/karabiner.edn
-_link $PATH_KARABINER_DOTFILES/karabiner $PWD/.config/karabiner
+if [ -f "$PWD/.config/karabiner.edn" ]; then
+  print -P "$fg[red] - Karabiner config already exists (skip)...$reset_color"
+else
+  mkdir -p $PWD/.config
+  _link $PATH_KARABINER_DOTFILES/karabiner.edn $PWD/.config/karabiner.edn
+  _link $PATH_KARABINER_DOTFILES/karabiner $PWD/.config/karabiner
+fi
